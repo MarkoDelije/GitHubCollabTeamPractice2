58306a2f24a009b4fcbfe80a7d2428835f9606a0
Marko'sMac <Markolucas93@yahoo.com>
2023-01-24 11:17:05 -0500

    TestCase001 Click Search Button Verification PASSED!!

---
+++ src/TestCase001_ClickSearchButton.java
@@ -0,0 +1,17 @@
+public class TestCase001_ClickSearchButton {
+
+    public static void main(String[] args) {
+
+        // located search bar successfully....
+
+        // clicked on a search bar successfully
+
+        // search bar asked for text input.
+
+        System.out.println("TestCase001 Click Search Button PASSED!!!");
+
+
+
+    }
+
+}
