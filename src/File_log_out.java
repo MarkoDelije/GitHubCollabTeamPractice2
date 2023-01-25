816da039363b8cf8c5dcb9b3fb17c1b82e73d7ac
Marvin_Cydeo <marvine77@gmail.com>
2023-01-24 21:28:09 -0800

    S1221 second attempt becuase i made changes

--- src/File_log_out.java
+++ src/File_log_out.java
@@ -3,7 +3,7 @@ public class File_log_out {
     public static void main(String[] args) {
 
 
-        // this is my log out functionality first commit
+        // this is my log out functionality first commit, this is my code for example
     }
 
 
